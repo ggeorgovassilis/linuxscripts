efd9f711f0eb810e0b7d26de85cf4ca76898ca31
Philip Branning <branning@gmail.com>
2019-11-22 22:18:52 -0800

    unsafe variable expansion is unsafe and broke macos

--- myip.sh
+++ myip.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Find the local IP by munging ipconfig output
+# Find the local IP by munging network tool output
 
 private_ip='(127\.|172\.|10\.)'
 ip_capture='(([0-9]{1,3}\.){3}[0-9]{1,3})'
@@ -9,28 +9,29 @@ ip_capture='(([0-9]{1,3}\.){3}[0-9]{1,3})'
 ipconfig=`command -v ipconfig`
 ifconfig=`command -v ifconfig`
 ip=`command -v ip`
-# windows has ipconfig but doesn't have ifconfig
-if [ -n $ipconfig ] && ! [ -n $ifconfig ]
-then
-    ipconfig \
-        | grep 'IPv4 Address' \
-        | egrep -v $private_ip \
-        | egrep -o $ip_pattern
+
 # linux has ip now
-elif [ -n $ip ]
+if [ -n "$ip" ]
 then
     ip addr show \
         | grep '^\s*inet[^6]' \
         | egrep -v $private_ip \
         | egrep -o $ip_capture \
         | head -n 1
+# windows has ipconfig but doesn't have ifconfig
+elif [ -n "$ipconfig" ] && ! [ -n "$ifconfig" ]
+then
+    ipconfig \
+        | grep 'IPv4 Address' \
+        | egrep -v $private_ip \
+        | egrep -o $ip_pattern
 # osx still has ifconfig
-elif [ -n $ifconfig ]
+elif [ -n "$ifconfig" ]
 then
     ifconfig \
         | grep '^\s*inet[^6]' \
-        | egrep -v '(127\.|172\.)' \
-        | egrep -o '(([0-9]{1,3}\.){3}[0-9]{1,3})'\
+        | egrep -v $private_ip \
+        | egrep -o $ip_capture \
         | head -n 1
 else
     echo 2>&1 "Must have ipconfig or ip or ifconfig available to get IP address"
