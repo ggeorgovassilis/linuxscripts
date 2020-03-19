e27c381b84430301c4f0c44d0020b47a1745e44a
George Georgovassilis <3505136+ggeorgovassilis@users.noreply.github.com>
2020-03-19 07:36:02 +0100

    Update update-proxy.sh

--- nordvpn-proxy/update-proxy.sh
+++ nordvpn-proxy/update-proxy.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+# update path with whereever the script is located
 cd /home/george/bin/scripts
 server=`./find-proxy.sh`
+echo $server
 cp haproxy.template haproxy.cfg
 sed -i "s/PLACEHOLDER/$server/g" haproxy.cfg
 cp haproxy.cfg /etc/haproxy
