b2697ee6e47a83b880b5b699386e35a69a2d564d
George Georgovassilis <3505136+ggeorgovassilis@users.noreply.github.com>
2020-03-19 07:35:03 +0100

    Update find-proxy.sh

--- nordvpn-proxy/find-proxy.sh
+++ nordvpn-proxy/find-proxy.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-proxy=`curl -s https://nordvpn.com/wp-admin/admin-ajax.php?action=servers_recommendations | jq -r ".[0].hostname"`
+# finds a SOCKS5 proxy in Sweden
+proxy=`curl 'https://nordvpn.com/wp-admin/admin-ajax.php?action=servers_recommendations&filters=\{%22country_id%22:208,%22servers_technologies%22:[7]\}' | jq -r '.[0].hostname'`
 echo $proxy
