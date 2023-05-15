099aa673646dd60ee6c199ea9868b835cf77dd39
George Georgovassilis <g.georgovassilis@gmail.com>
2023-05-15 20:23:44 +0300

    avoid starting if already running

--- touchpad-volume-control/touchpad-volume-control.sh
+++ touchpad-volume-control/touchpad-volume-control.sh
@@ -11,12 +11,22 @@ touchpad_device=/dev/input/event7
 
 # Volume change per touchpad movement event
 volume_d="1%"
-
 gesture_active=false
 previous_y=0
 
 regex="\\(ABS_Y\\), value ([0-9]+)"
 
+function abort_if_script_already_running (){
+
+  scriptname="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+
+  for pid in $(pidof -x "$scriptname"); do
+    if [ $pid != $$ ]; then
+        echo "[$(date)] : $scriptname : Process is already running with PID $pid"
+        exit 1
+    fi
+  done
+}
 
 function volume_up (){
   amixer --quiet -D pulse sset Master "$volume_d"+
@@ -28,13 +38,14 @@ function volume_down (){
 
 function process_line (){
 
-  if [[ $1 == *"(BTN_TOOL_TRIPLETAP), value 1"* ]]; then
+  line="$1"
+  if [[ "$line" == *"(BTN_TOOL_TRIPLETAP), value 1"* ]]; then
     gesture_active=true
-  elif [[ $1 == *"(BTN_TOOL_TRIPLETAP), value 0"* ]]; then
+  elif [[ "$line" == *"(BTN_TOOL_TRIPLETAP), value 0"* ]]; then
     gesture_active=false
   fi
 
-  if [[ $gesture_active == true && $1 =~ $regex ]]; then
+  if [[ $gesture_active == true && "$line" =~ $regex ]]; then
      y="${BASH_REMATCH[1]}"
      [[ $y -lt previous_y ]] && volume_up
      [[ $y -gt previous_y ]] && volume_down
@@ -46,10 +57,11 @@ function read_events (){
   
 # --line-buffered disables pipe buffering. Line buffering delays lines, so the read line loop doesn't see events in a timely manner
  
-  evtest "$touchpad_device" | grep --line-buffered 'BTN_TOOL_TRIPLETAP\|ABS_Y\|BTN_FINGER' | while read line; \
+  evtest "$touchpad_device" | grep --line-buffered 'BTN_TOOL_TRIPLETAP\|ABS_Y' | while read line; \
   do process_line "$line"; \
   done < "${1:-/dev/stdin}" 
 
 }
 
+abort_if_script_already_running
 read_events
